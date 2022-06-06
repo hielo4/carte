4148bde9d00f985221752a24f7762d19e441af88
ibrahim <djouadaibrahim@gmail.com>
2022-06-06 19:42:17 +0100

    table

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-var elements = document.getElementsByClassName("btn-pl");
-
-Array.from(elements).forEach(function(element) {
-    element.addEventListener('click', addqte);
-  });
-function addqte(){
-    var test = document.getElementById("test").innerHTML;
-    console.log(test)
-
-}
 
